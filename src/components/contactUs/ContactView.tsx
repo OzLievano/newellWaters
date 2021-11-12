5b5b42d844ee3d90b7f759ef1027ff7007d2018f
OzLievano <osvaldoalievano@gmail.com>
2021-11-12 16:32:37 -0500

    Added in contactUs component

---
+++ src/components/contactUs/ContactView.tsx
@@ -0,0 +1,52 @@
+import React, { useState, FunctionComponent } from 'react'
+
+export const ContactView:FunctionComponent = () => {
+    const [name, setName] = useState<string>('');
+    const [address, setAddress] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [phone, setPhone] = useState<string>('');
+    const [service, setService] = useState<string>('');
+
+    const handleChange = (e:any) => {
+        e.setState({value: e.target.value})
+    }
+
+    const handleSubmit = (e:any) => {
+        e.preventDefault();
+    }
+
+    return (
+        <div>
+            <form>
+              <label>
+                  Name:
+                  <input type="text"/> 
+              </label>
+              <label>
+                  Address:
+                  <input type="text"/>
+              </label>
+              <label>
+                  Email:
+                  <input type="text"/>
+              </label>
+              <label>
+                  Phone:
+                  <input type="text"/>
+              </label>
+              <label>
+                  Service:
+                <select id="services" name='services'>
+                    <option value="consultations">Consultations</option>
+                    <option value="concrete surfaces">Concrete Surfaces</option>
+                    <option value="wood surfaces">Wood Surfaces</option>
+                </select>
+              </label>
+              <label>
+                  Message:
+                  <textarea name="message" placeholder="please enter your request here"></textarea>
+              </label>
+            </form>
+        </div>
+    )
+}
